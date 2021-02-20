95102ce05e91315188c584d585b0abd392e286f6
Daniel <Jesus>
2021-02-20 15:44:02 +0000

    Criação de testes unitarios

---
+++ RestApiModeloDDD.Tests/MapperTests.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using NUnit.Framework;
+using RestApiModeloDDD.Application.Mappers;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RestApiModeloDDD.Tests
+{
+    [TestFixture]
+    [ExcludeFromCodeCoverage]
+    public class MapperTests
+    {
+        [Test]
+        public void AutoMapperDtoToModel_Configuration_IsValid()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<DtoToModelMappingCliente>());
+            config.AssertConfigurationIsValid();
+        }
+
+        [Test]
+        public void AutoMapperModelToDto_Configuration_IsValid()
+        {
+            var config = new MapperConfiguration(cfg => cfg.AddProfile<DtoToModelMappingCliente>());
+            config.AssertConfigurationIsValid();
+        }
+    }
+}
